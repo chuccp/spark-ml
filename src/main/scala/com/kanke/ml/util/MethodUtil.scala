27394086623f6f17a6cdc1fb46c1f1c5773082a4
tomcat <cooge123@gmail.com>
2023-03-03 14:48:56 +0800

    Update MethodUtil.scala

--- src/main/scala/com/kanke/ml/util/MethodUtil.scala
+++ src/main/scala/com/kanke/ml/util/MethodUtil.scala
@@ -3,8 +3,11 @@ package com.kanke.ml.util
 import com.kanke.ml.annotation.RTask
 import org.springframework.web.context.WebApplicationContext
 
+import java.util.concurrent.Executors
+
 object MethodUtil {
 
+  val pool = Executors.newFixedThreadPool(5)
 
   def execute(context: WebApplicationContext, name: String, args: Array[String]): Unit = {
     val names = context.getBeanDefinitionNames
@@ -15,11 +18,15 @@ object MethodUtil {
         if (rTask != null) {
           if (args == null || args.isEmpty) {
             if (v.getParameterCount == 0) {
-              v.invoke(obj)
+              pool.execute(()=>{
+                v.invoke(obj)
+              })
             }
           } else {
             if (v.getParameterCount == args.length) {
-              v.invoke(obj, args: _*)
+              pool.execute(() => {
+                v.invoke(obj, args: _*)
+              })
             }
           }
         }
